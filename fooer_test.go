37c1ea484e9c405c60887ca9b3ac979bd08ff217
Alexandre Couedelo <nokwebspace@gmail.com>
2022-09-19 20:02:47 +0000

    feat: t.Fatalf vs t.Errorf

--- fooer_test.go
+++ fooer_test.go
@@ -8,3 +8,18 @@ func TestFooer(t *testing.T) {
 		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
 	}
 }
+
+func TestFooer2(t *testing.T) {
+	input := 3
+	result := Fooer(3)
+
+	t.Logf("The input was %d", input)
+
+	if result != "Foo" {
+		t.Errorf("Result was incorrect, got: %s, want: %s.", result, "Foo")
+	}
+
+	t.Fatalf("Stop the test now, we have seen enough")
+
+	t.Error("This won't be executed")
+}
